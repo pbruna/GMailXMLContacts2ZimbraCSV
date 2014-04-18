4dc97c1304eb6987f58ebc5989c1b4bdfe932f0e
Patricio Bruna <pbruna@gmail.com>
2014-04-18 17:11:29 -0300

    Working

--- Readme.md
+++ Readme.md
@@ -1,6 +1,37 @@
 ### Introducction
 This is a Ruby script that read and parse a GMail XML contact file and returns a CSV file to be imported with the Zimbra Webmail.
 
-### Google reference
+This is a 0.1 version, meaning it just do what I need it to do right now, nothing Fancy yet.
+
+### Google Reference
 The information needed to make the translation from Google to Zimbra is located here https://developers.google.com/gdata/docs/2.0/elements
+
+This is what is used in the ``Z2G_DICTIONARY`` constant Hash.
+
+### How to use it
+
+1. Clone this repo
+```bash
+git clone git@github.com:pbruna/GMailXMLContacts2ZimbraCSV.git
+```
+
+2. Run bundle inside the new directory to install what you need
+```bash
+bundle
+```
+
+3. Convert the #%%$! XML Google File
+```bash
+ruby gc2zc.rb contacts.xml
+```
+
+4. You should get a new file called ```contacts.csv```
+
+### TODOs
+Here is what I should start working next:
+
+1. Testing, yes, testing
+2. Auto-write (Metaprograming) the methods of GmailContact with method_missing
+3. Check that the file passed is actually a XML file
+4. Ask for help
 
--- gc2zc.rb
+++ gc2zc.rb
@@ -1,27 +1,36 @@
 require 'nokogiri'
 require 'open-uri'
 require 'csv'
+require 'pp'
 
 ZIMBRA_CSV_FIELDS = ["Title","FirstName","MiddleName","LastName","Suffix","Company","Department","JobTitle","BusinessStreet","BusinessCity","BusinessState","BusinessPostalCode","BusinessCountry","HomeStreet","HomeCity","HomeState","HomePostalCode","HomeCountry","OtherStreet","OtherCity","OtherState","OtherPostalCode","OtherCountry","AssistantsPhone","BusinessFax","BusinessPhone","BusinessPhone2","Callback","CarPhone","CompanyMainPhone","HomeFax","HomePhone","HomePhone2","ISDN","MobilePhone","OtherFax","OtherPhone","Pager","PrimaryPhone","RadioPhone","TTYTDDPhone","Telex","Account","Anniversary","AssistantsName","BillingInformation","Birthday","Categories","Children","DirectoryServer","EmailAddress","EmailDisplayName","Email2Address","Email2DisplayName","Email3Address","Email3DisplayName","Gender","GovernmentIDNumber","Hobby","Initials","InternetFreeBusy","Keywords","Language1","Location","ManagersName","Mileage","Notes","OfficeLocation","OrganizationalIDNumber","POBox","Priority","Private","Profession","ReferredBy","Sensitivity","Spouse","User 1","User 2","User 3","User 4","Web Page"]
 
 Z2G_DICTIONARY = {
-  "FirstName" => {"gd:name" => "gd:givenName"},
-  "MiddleName" => {"gd:name" => "gd:additionalName"},
-  "LastName" => {"gd:name" => "gd:familyName"},
-  "BusinessPhone" => "gd:phoneNumber",
-  "EmailAddress" => "gd:email#address"
+  "FirstName" => "givenName",
+  "MiddleName" => "additionalName",
+  "LastName" => "familyName",
+  "BusinessPhone" => "phoneNumber",
+  "EmailAddress" => "email"
 }
 
 
 class GmailXMLParser
-  attr_reader :contacts
+  attr_reader :contacts_xml
   
   def initialize(gmail_xml_contact_data)
-    contacts = Nokogiri.XML(open(gmail_xml_contact_data))
+    @contacts_xml = parse_and_return_entries(gmail_xml_contact_data)
+  end
+  
+  def contacts
+    contacts = Array.new
+    @contacts_xml.each do |contact_xml|
+      contacts << GmailContact.new(contact_xml)
+    end
+    return contacts
   end
   
   private
-  def parse_and_return_entries
+  def parse_and_return_entries(gmail_xml_contact_data)
     document = Nokogiri.XML(open(gmail_xml_contact_data))
     document.xpath('//xmlns:feed/xmlns:entry')
   end
@@ -32,23 +41,60 @@ class GmailContact
   attr_accessor :contact_xml
   
   def initialize(contact_xml)
-    contact_xml = contact_xml
+    @contact_xml = contact_xml
+  end
+  
+  def additionalName
+    ""
   end
   
   def email
-    contact_xml.xpath(".//gd:email").first["address"]
+    return "" if @contact_xml.xpath(".//gd:email").first.nil?
+    @contact_xml.xpath(".//gd:email").first["address"]
   end
   
   def phoneNumber
-    contact_xml.xpath(".//gd:phoneNumber").text
+    @contact_xml.xpath(".//gd:phoneNumber").text
   end
   
   def givenName
-    contact_xml.xpath(".//gd:name/gd:givenName").text
+    @contact_xml.xpath(".//gd:name/gd:givenName").text
   end
   
   def familyName
-    contact_xml.xpath(".//gd:name/gd:givenName").text
+    @contact_xml.xpath(".//gd:name/gd:familyName").text
   end
   
 end
+
+# main program
+
+# Parse the XML with the contacts
+# ARGV[0] is the file
+contacts = GmailXMLParser.new(ARGV[0]).contacts
+
+# build a hash with the values of the ZIMBRA_CSV_FIELDS constant ARRAYS as keys
+# Remember that from Ruby 1.9 Hash are ordered, nice!!
+zimbra_csv_hash = Hash.new
+ZIMBRA_CSV_FIELDS.each do |el|
+  zimbra_csv_hash[el] = ""
+end
+
+# Lets build the Array of contacts for the CSV File
+contacts_array = []
+contacts.each do |contact|
+  Z2G_DICTIONARY.each do |key,value|
+    zimbra_csv_hash[key] = contact.send(value)
+  end
+  contacts_array << zimbra_csv_hash.values
+end
+
+# Lets write the CSV file with the same name of the original file + .csv
+filename = File.basename(ARGV[0], ".*")
+CSV.open("#{filename}.csv", "wb", {force_quotes: true} ) do |csv|
+  csv << ZIMBRA_CSV_FIELDS
+  contacts_array.each do |hash|
+    csv << hash
+  end
+end
+
